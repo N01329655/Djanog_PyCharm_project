f9601fa2debf1913982165d4411669a33e78d4ea
George <georgenechyporenko@gmail.com>
2021-05-15 14:29:10 -0700

    second commit

--- book_app01/urls.py
+++ book_app01/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', homePageView, name='homepage')
-
+    path('', HomePageView.as_view(), name='homepage'),
+    path('about/', AboutPageView.as_view(), name='aboutpage'),
 
 
 ]
